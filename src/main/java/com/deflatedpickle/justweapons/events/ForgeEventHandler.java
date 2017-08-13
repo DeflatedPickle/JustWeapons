cf51a6c77e3d9710c7088c7674d2375e4ee521b2
DeflatedPickle <DeflatedPickle@gmail.com>
2017-08-13 05:02:01 +0100

    War Axes now ignore armour

--- src/main/java/com/deflatedpickle/justweapons/events/ForgeEventHandler.java
+++ src/main/java/com/deflatedpickle/justweapons/events/ForgeEventHandler.java
@@ -1,17 +1,23 @@
 package com.deflatedpickle.justweapons.events;
 
 import com.deflatedpickle.justweapons.items.ItemDagger;
+import com.deflatedpickle.justweapons.items.ItemWarAxe;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.Item;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ForgeEventHandler {
     @SubscribeEvent
     public void onLivingHurtEvent(LivingHurtEvent event){
-        if (((EntityLivingBase) event.getSource().getSourceOfDamage()).getHeldItemMainhand().getItem() instanceof ItemDagger) {
-            if (event.getEntity().getHorizontalFacing().equals(event.getSource().getSourceOfDamage().getHorizontalFacing())) {
+        Item heldItem = ((EntityLivingBase) event.getSource().getSourceOfDamage()).getHeldItemMainhand().getItem();
+        if (heldItem instanceof ItemDagger) {
+            if (event.getSource().getSourceOfDamage().isSneaking() && event.getEntity().getHorizontalFacing().equals(event.getSource().getSourceOfDamage().getHorizontalFacing())) {
                 event.setAmount(event.getAmount() + 4);
             }
+        }
+        else if (heldItem instanceof ItemWarAxe) {
+            event.getSource().setDamageBypassesArmor();
         }
     }
 }
