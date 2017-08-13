112ffad39ef7490c02f952f7a74d010b2d2c6342
DeflatedPickle <DeflatedPickle@gmail.com>
2017-08-13 03:59:49 +0100

    Fixed #1

--- src/main/java/com/deflatedpickle/justweapons/events/ForgeEventHandler.java
+++ src/main/java/com/deflatedpickle/justweapons/events/ForgeEventHandler.java
@@ -1,13 +1,17 @@
 package com.deflatedpickle.justweapons.events;
 
+import com.deflatedpickle.justweapons.items.ItemDagger;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ForgeEventHandler {
     @SubscribeEvent
     public void onLivingHurtEvent(LivingHurtEvent event){
-        if (event.getEntity().getHorizontalFacing().equals(event.getSource().getSourceOfDamage().getHorizontalFacing())) {
-            event.setAmount(event.getAmount() + 4);
+        if (((EntityLivingBase) event.getSource().getSourceOfDamage()).getHeldItemMainhand().getItem() instanceof ItemDagger) {
+            if (event.getEntity().getHorizontalFacing().equals(event.getSource().getSourceOfDamage().getHorizontalFacing())) {
+                event.setAmount(event.getAmount() + 4);
+            }
         }
     }
 }
