c1a88609375f887900d2fcd7abfa24e698fd4861
DeflatedPickle <DeflatedPickle@gmail.com>
2017-08-13 07:20:09 +0100

    Added recipes

--- src/main/java/com/deflatedpickle/justweapons/init/ModCrafting.java
+++ src/main/java/com/deflatedpickle/justweapons/init/ModCrafting.java
@@ -5,10 +5,25 @@ import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class ModCrafting {
     public static void register(){
-        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.WOODEN_DAGGER, "   ", " M ", " S ", 'M', "plankWood", 'S', "stickWood"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.STONE_DAGGER, "   ", " M ", " S ", 'M', "cobblestone", 'S', "stickWood"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.IRON_DAGGER, "   ", " M ", " S ", 'M', "ingotIron", 'S', "stickWood"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.GOLD_DAGGER, "   ", " M ", " S ", 'M', "ingotGold", 'S', "stickWood"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.DIAMOND_DAGGER, "   ", " M ", " S ", 'M', "gemDiamond", 'S', "stickWood"));
+        // Daggers
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.WOODEN_DAGGER, "M", "S", 'M', "plankWood", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.STONE_DAGGER, "M", "S", 'M', "cobblestone", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.IRON_DAGGER, "M", "S", 'M', "ingotIron", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.GOLD_DAGGER, "M", "S", 'M', "ingotGold", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.DIAMOND_DAGGER, "M", "S", 'M', "gemDiamond", 'S', "stickWood"));
+
+        // War Axes
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.WOODEN_WARAXE, "MM ", "MSM", " S ", 'M', "plankWood", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.STONE_WARAXE, "MM ", "MSM", " S ", 'M', "cobblestone", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.IRON_WARAXE, "MM ", "MSM", " S ", 'M', "ingotIron", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.GOLD_WARAXE, "MM ", "MSM", " S ", 'M', "ingotGold", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.DIAMOND_WARAXE, "MM ", "MSM", " S ", 'M', "gemDiamond", 'S', "stickWood"));
+
+        // Maces
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.WOODEN_MACE, " MM", " SM", "S  ", 'M', "plankWood", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.STONE_MACE, " MM", " SM", "S  ", 'M', "cobblestone", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.IRON_MACE, " MM", " SM", "S  ", 'M', "ingotIron", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.GOLD_MACE, " MM", " SM", "S  ", 'M', "ingotGold", 'S', "stickWood"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(ModItems.DIAMOND_MACE, " MM", " SM", "S  ", 'M', "gemDiamond", 'S', "stickWood"));
     }
 }
